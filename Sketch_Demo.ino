e28f603e4f988645cfa0f1e8056b331acbde840e
Building Projects <35693082+TeamWorkBP@users.noreply.github.com>
2018-01-22 12:57:29 -0500

    Add files via upload

---
+++ Sketch_Demo.ino
@@ -0,0 +1,21 @@
+//Inclusión de librerias
+
+//Definición de variables
+
+//Configuración de la placa
+void setup() {
+  //Coloca aquí tu código de configuracion.
+  //Solo se ejecutará una sola vez.
+}
+
+//Bucle principal
+void loop() {
+  //Coloca aquí tu código principal.
+  //Se ejecutará periodicamente.
+}
+
+//Funciones
+void function(){
+  //Funciones Personales
+}
+
