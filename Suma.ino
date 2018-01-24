51b963c6c3b7afdf863a8f208a2fe2adb6b39b7c
Building Projects <35693082+TeamWorkBP@users.noreply.github.com>
2018-01-24 16:24:39 -0500

    Add files via upload

---
+++ Suma.ino
@@ -0,0 +1,28 @@
+//Inclusión de librerias
+
+//Definición de variables
+int num1;
+int num2;
+int num3;
+
+//Configuración de la placa
+void setup() {
+  //Coloca aquí tu código de configuracion.
+  //Solo se ejecutará una sola vez.
+}
+
+//Bucle principal
+void loop() {
+  //Coloca aquí tu código principal.
+  //Se ejecutará periodicamente.
+  num3 = num1+num2;
+  if(num3 >= 5 ){
+    num3 = num1-num2;
+  }
+}
+
+//Funciones
+void function(){
+  //Funciones Personales
+}
+
